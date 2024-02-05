2e197987e35e3aa64ac585dbf97eb445e538176c
Козин Андрей <a.kozin@sbereducation.ru>
2024-02-05 17:26:04 +0300

    edit front end, add new controllers

--- george/src/main/java/com/georgeCross/george/controllers/SearchController.java
+++ george/src/main/java/com/georgeCross/george/controllers/SearchController.java
@@ -1,22 +1,23 @@
 package com.georgeCross.george.controllers;
 
-
-import com.georgeCross.george.models.Product;
 import com.georgeCross.george.service.ProductService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequiredArgsConstructor
-public class ProductController {
+@Api("Контроллер Swagger")
+
+public class SearchController {
     private final ProductService productService;
 
-    @GetMapping("/")
+    @GetMapping("/search")
+    @ApiOperation("поиск по номеру")
     public String products(@RequestParam(name = "title", required = false) String title, Model model) {
         model.addAttribute("products", productService.getListFindByTitle(title));
         return "search";
