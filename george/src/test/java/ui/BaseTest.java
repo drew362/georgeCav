9c5d4f0af8ef4a82c70bbfc639b763efe2cf6291
Козин Андрей <a.kozin@sbereducation.ru>
2024-02-28 18:29:11 +0300

    добавил настройки для jenkins

--- george/src/test/java/ui/BaseTest.java
+++ george/src/test/java/ui/BaseTest.java
@@ -10,7 +10,8 @@ import org.junit.Before;
 abstract public class BaseTest {
     public void setUp() {
         Configuration.browser = "chrome";
-        Configuration.browserBinary="/Users/a.kozin/Downloads/chromedriver";
+        System.setProperty("webdriver.chrome.driver","/Users/a.kozin/Downloads/chromedriver");
+//        Configuration.browserBinary="/Users/a.kozin/Downloads/chromedriver";
         Configuration.browserSize = "1920x1080";
         SelenideLogger.addListener("AllureSelenide",new AllureSelenide());
         Configuration.headless = false;
