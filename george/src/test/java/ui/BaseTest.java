f9470797f098f30c46290569c6495279c40e0154
Козин Андрей <a.kozin@sbereducation.ru>
2024-02-29 12:36:28 +0300

    refactor

--- george/src/test/java/ui/BaseTest.java
+++ george/src/test/java/ui/BaseTest.java
@@ -3,6 +3,7 @@ package ui;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.After;
 import org.junit.Before;
@@ -10,6 +11,7 @@ import org.junit.Before;
 abstract public class BaseTest {
     public void setUp() {
         Configuration.browser = "chrome";
+        WebDriverManager.chromedriver().setup();
         System.setProperty("webdriver.chrome.driver","/Users/a.kozin/Downloads/chromedriver");
 //        Configuration.browserBinary="/Users/a.kozin/Downloads/chromedriver";
         Configuration.browserSize = "1920x1080";
