d43a105770105b8b89de0b822c46ef4e2d49e8e3
drew <kozinandrey@mail.ru>
2025-04-03 22:40:34 +0300

    add error message for empty rq

--- george/src/main/java/com/georgeCross/george/controllers/SearchController.java
+++ george/src/main/java/com/georgeCross/george/controllers/SearchController.java
@@ -1,13 +1,19 @@
 package com.georgeCross.george.controllers;
 
+import com.georgeCross.george.models.Product;
 import com.georgeCross.george.service.ProductService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,9 +25,17 @@ public class SearchController {
 
     @GetMapping("/search")
     @ApiOperation("поиск по номеру")
-    public String searchProducts(@RequestParam(name = "query",required=false) String query, Model model) {
+    public String searchProducts(@RequestParam(name = "query", required = false) String query, Model model) {
 
-            model.addAttribute("products", productService.getListFindByNumberOrName(query));
-            return "search";
+        List<Product> list = Collections.emptyList();
+
+        if (StringUtils.hasText(query)) {
+            list = productService.getListFindByNumberOrName(query);
+            if (list.isEmpty()) {
+                model.addAttribute("message", "Ничего не найдено");
+            }
+        }
+        model.addAttribute("products", list);
+        return "search";
     }
 }
