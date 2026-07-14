bb00a64d33472fdb73947f63be1a83590bd5887b
drew <kozinandrey@mail.ru>
2026-07-14 14:22:23 +0300

    new controller

--- george/src/main/java/com/georgeCross/george/controllers/ShopController.java
+++ george/src/main/java/com/georgeCross/george/controllers/ShopController.java
@@ -5,9 +5,11 @@ import com.georgeCross.george.repositories.ProductRepository;
 
 import com.georgeCross.george.service.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -42,4 +44,60 @@ public class ShopController {
 
         return savedProduct;
     }
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<Void> deleteProductById(@PathVariable("id") Long id) {
+        productRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<Product> updateProduct(
+            @PathVariable("id") Long id,
+            @ModelAttribute Product productDetails,
+            @RequestParam(value = "file",
+                    required = false) MultipartFile[] files) {
+        return productRepository.findById(id).map(existingProduct -> {
+            existingProduct.setTitle(productDetails.getTitle());
+            existingProduct.setDescription(productDetails.getDescription());
+            existingProduct.setPrice(productDetails.getPrice());
+
+            if (files != null && files.length > 0 && !files[0].isEmpty()) {
+                List<String> newCloudImageUrls = imageService.uploadProductImage(files, existingProduct.getId());
+                List<String> currentUrls = existingProduct.getImageUrls() != null
+                        ? new ArrayList<>(existingProduct.getImageUrls())
+                        : new ArrayList<>();
+
+                currentUrls.addAll(newCloudImageUrls);
+
+                existingProduct.setImageUrls(currentUrls);
+            }
+
+            Product updateProduct = productRepository.save(existingProduct);
+            return ResponseEntity.ok(updateProduct);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/products/{id}/images")
+    public ResponseEntity<Product> deleteImg(
+            @PathVariable("id") Long id,
+            @RequestParam("imgUrl") String imageUrl
+    ) {
+        return productRepository.findById(id).map(product -> {
+            List<String> currentUrls = product.getImageUrls();
+
+            if (currentUrls != null && currentUrls.contains(imageUrl)) {
+                imageService.deleteProductImageFromCloud(imageUrl);
+
+                currentUrls.remove(imageUrl);
+                product.setImageUrls(currentUrls);
+
+                Product newUpdateProduct = productRepository.save(product);
+                return ResponseEntity.ok(newUpdateProduct);
+            }
+            return ResponseEntity.badRequest().body(product);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+
 }
--- george/src/main/java/com/georgeCross/george/service/ImageService.java
+++ george/src/main/java/com/georgeCross/george/service/ImageService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
@@ -27,9 +28,11 @@ public class ImageService {
     public List<String> uploadProductImage(MultipartFile[] files, Long productsId) {
         List<String> uploadedUrls = new ArrayList<>();
 
+        if (files == null || files.length > 0)
+            return uploadedUrls;
+
         for (MultipartFile file : files) {
             if (file.isEmpty()) continue;
-
 
             String fileExtension = getFileExtension(file.getOriginalFilename());
             String fileName = "products/" + productsId + "/" + UUID.randomUUID() + fileExtension;
@@ -50,11 +53,32 @@ public class ImageService {
                 throw new RuntimeException("not load foto");
             }
         }
-            return uploadedUrls;
-        }
+        return uploadedUrls;
+    }
 
-        private String getFileExtension (String fileName){
-            if (fileName == null || !fileName.contains(".")) return ".jpg";
-            return fileName.substring(fileName.lastIndexOf("."));
+    private String getFileExtension(String fileName) {
+        if (fileName == null || !fileName.contains(".")) return ".jpg";
+        return fileName.substring(fileName.lastIndexOf("."));
+    }
+
+    public void deleteProductImageFromCloud(String imgUrls) {
+        try {
+
+            String backetMark = ".net/" + s3Config.getBucketName() + "/";
+            if (!imgUrls.contains(backetMark)) {
+                return;
+            }
+            String keyInFile = imgUrls.substring(imgUrls.indexOf(backetMark) + backetMark.length());
+
+            DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
+                    .bucket(s3Config.getBucketName())
+                    .key(keyInFile)
+                    .build();
+
+            s3Client.deleteObject(deleteObjectRequest);
+
+        } catch (Exception e) {
+            System.err.println("Не удалось удалить файл из Yandex Cloud: " + e.getMessage());
         }
     }
+}
